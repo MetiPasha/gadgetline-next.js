e055cba7bcc51a8b54ce0e810a1f584f44f4d1f4
pasha <mtipasha2021@gmail.com>
2025-02-04 19:48:57 +0330

    login fixed

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation";
 import { AUTH_BASE_URL } from "@/config.server";
 import { formDataToObject } from "@/lib/utils";
 
-export async function loginAction(state: LoginFormState, formData: FormData) {
+export async function login(state: LoginFormState, formData: FormData) {
   /// validate input
   const validatedFields = LoginFormSchema.safeParse(formDataToObject(formData));
   if (!validatedFields.success) {
