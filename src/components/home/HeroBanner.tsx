314b01bcf30cfe8223593bd6ec692719d11ff8d9
pasha <mtipasha2021@gmail.com>
2025-02-13 02:20:51 +0330

    2 banners component added for homepage

---
+++ src/components/home/HeroBanner.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { Box } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import { Autoplay, EffectFade } from "swiper/modules";
+
+const images = [
+  "/assets/home/Hbanner1.webp",
+  "/assets/home/Hbanner2.webp",
+  "/assets/home/Hbanner3.webp",
+  "/assets/home/Hbanner4.webp",
+  "/assets/home/Hbanner5.webp",
+];
+
+const HeroBanner = () => {
+  return (
+    <Swiper
+      modules={[EffectFade, Autoplay]}
+      effect="fade"
+      autoplay={{ delay: 2000, disableOnInteraction: false }}
+      loop={true}
+      style={{ width: "100%", height: "100%", marginTop: "5px" }}
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <Box
+            sx={{
+              width: "100%",
+              height: { xs: 300, md: 400 },
+              backgroundImage: `url(${image})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          ></Box>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default HeroBanner;
---
+++ src/components/home/HomeBanners.tsx
@@ -0,0 +1,33 @@
+import { Grid, Card, CardMedia, Container } from "@mui/material";
+
+const HomeBanners = () => {
+  return (
+    <Container maxWidth="lg" sx={{ mb: 15 }}>
+      <Grid container spacing={2} justifyContent="center" sx={{ mt: 4 }}>
+        <Grid item xs={12} sm={10} md={6}>
+          <Card sx={{ position: "relative", maxWidth: "100%", margin: "auto" }}>
+            <CardMedia
+              component="img"
+              height="250"
+              image="/assets/home/banner1.webp"
+              alt="بنر تخفیف لوازم آرایشی"
+            />
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={10} md={6}>
+          <Card sx={{ position: "relative", maxWidth: "100%", margin: "auto" }}>
+            <CardMedia
+              component="img"
+              height="250"
+              image="/assets/home/banner1.webp"
+              alt="بنر تخفیف کرم ضد آفتاب"
+            />
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default HomeBanners;
